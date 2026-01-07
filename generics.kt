84ad7f1ab2423874fe474867bd69d8b7a6c518fb
btbare <btbare2002@gmail.com>
2026-01-06 21:23:27 -0500

    testing

--- generics.kt
+++ generics.kt
@@ -9,4 +9,4 @@ fun main() {
     val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
     val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 	println(question1.answer)
-}
+} 
